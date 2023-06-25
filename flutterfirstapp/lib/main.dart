6d75650f46db0a8b8525b32c212611ce0587a970
hyun0914 <gus443@naver.com>
2023-06-26 00:42:39 +0900

    splash 화면 추가 및 수정(ios, aos)둘다 적용 flutter_native_splash 패키지 사용

--- flutterfirstapp/lib/main.dart
+++ flutterfirstapp/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'main_page.dart';
 
 void main() {
-  Future.delayed(const Duration(milliseconds: 140));
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  FlutterNativeSplash.remove();
   runApp(
     const MaterialApp(
       home: MainPage(),
